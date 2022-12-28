b207ad445e2527edfb8b31ff57d8c3f63076d75d
Lavanya Reddy <113541450+lavanaya4435@users.noreply.github.com>
2022-12-28 10:18:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,10 +11,10 @@ document.getElementById(index).style.backgroundColor="rgb("+r+","+g+","+b+")";
 
 arr.splice(index,1);
 
-// var r1= Math.floor((Math.random())*256);
-// var g1= Math.floor((Math.random())*256);
-// var b1= Math.floor((Math.random())*256);
-// document.getElementById(arr[0]).style.backgroundColor="rgb("+r1+","+g1+","+b1+")";
+var r1= Math.floor((Math.random())*256);
+var g1= Math.floor((Math.random())*256);
+var b1= Math.floor((Math.random())*256);
+document.getElementById(arr[0]).style.backgroundColor="rgb("+r1+","+g1+","+b1+")";
 
 var r2= Math.floor((Math.random())*256);
 var g2= Math.floor((Math.random())*256);
